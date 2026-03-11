b9915ece5ce1c843731b32af1b285e79b9d0d4d9
SetarehDew <sdymonds21@gmail.com>
2026-03-10 23:24:46 -0500

    Update server.js
    another edit to see it code is committed to GitHub

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ app.get("/version", (req, res) => {
 });
 const express = require("express");
 const braintree = require("braintree");
-
 const app = express();
 app.use(express.json());
 
+// Configure Braintree Gateway
 const gateway = new braintree.BraintreeGateway({
   environment: braintree.Environment.Sandbox,
   merchantId: process.env.BRAINTREE_MERCHANT_ID,
@@ -14,11 +14,9 @@ const gateway = new braintree.BraintreeGateway({
   privateKey: process.env.BRAINTREE_PRIVATE_KEY
 });
 
-app.get("/", (req, res) => {
-  res.send("Server running");
-});
-
-// <-- THIS IS THE IMPORTANT TOKEN ROUTE
+// Routes
+app.get("/", (req, res) => res.send("Server running"));
+app.get("/version", (req, res) => res.send("Latest version deployed!"));
 app.get("/token", async (req, res) => {
   try {
     const response = await gateway.clientToken.generate({});
@@ -29,33 +27,5 @@ app.get("/token", async (req, res) => {
   }
 });
 
-app.post("/checkout", async (req, res) => {
-  const nonce = req.body.nonce;
-  try {
-    const result = await gateway.transaction.sale({
-      amount: "10.00",
-      paymentMethodNonce: nonce,
-      options: { submitForSettlement: true }
-    });
-    res.send(result);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error processing transaction");
-  }
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running on port " + PORT));
-
-app.get("/", (req, res) => {
-  res.send("Server running");
-});
-
-app.get("/version", (req, res) => {
-  res.send("Latest version deployed!");
-});
-
-app.get("/token", async (req, res) => {
-  const response = await gateway.clientToken.generate({});
-  res.send(response.clientToken);
-});
